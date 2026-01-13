f47f7a3b9f12772c918cef37710742efc8b4b4cc
shaam-ex <ahtishamahmed1234@gmail.com>
2026-01-13 18:02:58 +0500

    Origin Update

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ const fastify = Fastify({ logger: true });
 // Register the main /api router
 fastify.register(apiRouter, { prefix: "/api" });
 fastify.register(cors, {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
