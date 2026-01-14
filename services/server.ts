736735b955b52cec8d5b468ba2f3ec2b4de74d46
shaam-ex <ahtishamahmed1234@gmail.com>
2026-01-14 15:07:12 +0500

    Improvements

--- services/server.ts
+++ services/server.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+const deploymentServiceUrl =
+  process.env.DEPLOYMENT_SERVER || "http://localhost:4000";
+const defaultMemory = process.env.DEFAULT_MEMORY || "1g";
+const defaultCPU = process.env.DEFAULT_CPU || "1";
 
 export async function getAllServers() {
   try {
@@ -13,6 +17,15 @@ export async function getAllServers() {
     return { success: false, message: "Failed to fetch servers" };
   }
 }
+
+type deploymentServerCreateResponse = {
+  success: boolean;
+  message: string;
+  data: {
+    port: number;
+    ipAddress: string;
+  };
+};
 
 export async function createServer(
   name: string,
@@ -32,6 +45,31 @@ export async function createServer(
         type,
       },
     });
+    // Calling Deployment Service to actually deploy the server
+    const response: Response = await fetch(`${deploymentServiceUrl}/deploy`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        containerName: name,
+        type,
+        version,
+        memory: defaultMemory,
+        cpus: defaultCPU,
+        // Will add more configuration options later
+        // configuration,
+      }),
+    });
+
+    const deploymentResponse =
+      (await response.json()) as deploymentServerCreateResponse;
+
+    if (!deploymentResponse.success) {
+      // If deployment fails, we should rollback the created server entry
+      await prisma.server.delete({ where: { id: server.id } });
+      return { success: false, message: "Failed to deploy server" };
+    }
     return {
       success: true,
       message: "Server created successfully",
@@ -40,8 +78,8 @@ export async function createServer(
         status: server.status,
         configuration: server.configuration,
         version: server.version,
-        ipAddress: server.ipAddress,
-        port: server.port,
+        ipAddress: deploymentResponse.data.ipAddress,
+        port: deploymentResponse.data.port,
       },
     };
   } catch (error) {
