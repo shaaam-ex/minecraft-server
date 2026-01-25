77d5a9562b15456d8ffa2120fbb6691c72f23fa8
shaaam-ex <ahtishamahmed1234@gmail.com>
2026-01-25 16:22:19 +0500

    Centralized Prisma

--- services/user.ts
+++ services/user.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { prisma } from "../prisma/client";
 
 export async function getUserProfile(userId: number) {
   try {
